7a92e4e92cbe33f797271e1f783e9e3579cc6ea7
natharduini <nathalie.arduini@gmail.com>
2014-10-20 16:04:52 +0200

    removed the need for moment.js when showWeek=true

--- js/bootstrap-datetimepicker.js
+++ js/bootstrap-datetimepicker.js
@@ -621,8 +621,7 @@
 				
 				if (prevMonth.getUTCDay() == this.weekStart) {
 					if (this.showWeek) {
-						var tmp = moment(prevMonth);
-						html.push('<tr><td class="week_nr"><div class="number">'+tmp.week()+'<div></td>');
+						html.push('<tr><td class="week_nr"><div class="number">'+this.getWeekNumber(prevMonth)+'<div></td>');
 					} else {
 						html.push('<tr>');
 					}
@@ -1347,6 +1346,36 @@
 				case 0:
 					return 'hour';
 			}
+		},
+		/* For a given date, get the ISO week number
+		 *
+		 * Based on information at:
+		 *
+		 *    http://www.merlyn.demon.co.uk/weekcalc.htm#WNR
+		 *
+		 * Algorithm is to find nearest thursday, it's year
+		 * is the year of the week number. Then get weeks
+		 * between that date and the first day of that year.
+		 *
+		 * Note that dates in one year can be weeks of previous
+		 * or next year, overlap is up to 3 days.
+		 *
+		 * e.g. 2014/12/29 is Monday in week  1 of 2015
+		 *      2012/1/1   is Sunday in week 52 of 2011
+		 */
+		getWeekNumber: function(d) {
+		    // Copy date so don't modify original
+		    d = new Date(+d);
+		    d.setHours(0,0,0);
+		    // Set to nearest Thursday: current date + 4 - current day number
+		    // Make Sunday's day number 7
+		    d.setDate(d.getDate() + 4 - (d.getDay()||7));
+		    // Get first day of year
+		    var yearStart = new Date(d.getFullYear(),0,1);
+		    // Calculate full weeks to nearest Thursday
+		    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+		    // Return array of year and week number
+		    return weekNo;
 		}
 	};
 
